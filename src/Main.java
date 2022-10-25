b8634925978dbe9a0495920a1235c44b977a7b95
kamen <maks.kolesnikov.0202@mail.ru>
2022-10-25 21:29:04 +0300

    Objects and classes

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,14 @@ public class Main {
         System.out.println("sarah.getAge() = " + sarah.getAge());
         Person john = new Person("John", 13);
 
+
+        Book stalker = new Book("S.T.A.L.K.E.R.", 2007);
+        Author sergey = new Author("Sergey", "Nedorubov");
+        System.out.println(stalker.getName() + " " + stalker.getYearPublication() + " " + sergey.getName() + " " + sergey.getMiddleName());
+        Book witcher = new Book("Witcher", 1986);
+        Author andrzej = new Author("Andrzej", "Sapkowski");
+        System.out.println(witcher.getName() + " " + witcher.getYearPublication() + " " + andrzej.getName() + " " + andrzej.getMiddleName());
+        witcher.setYearPublication(2013);
+        System.out.println(witcher.getName() + " " + witcher.getYearPublication() + " " + andrzej.getName() + " " + andrzej.getMiddleName());
     }
 }

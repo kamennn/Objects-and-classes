260e31f0d2458f39cce85333ff960387d648306d
kamen <maks.kolesnikov.0202@mail.ru>
2022-10-26 20:57:05 +0300

    Objects and classes

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,20 @@
 public class Book {
+    private String nameAuthor;
     private String name;
     private int yearPublication;
 
-    public Book(String name, int yearPublication) {
+    public Book(String name,String nameAuthor, int yearPublication) {
         this.name = name;
         this.yearPublication = yearPublication;
+        this.nameAuthor = nameAuthor;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String getNameAuthor() {
+        return this.nameAuthor;
     }
 
     public int getYearPublication() {

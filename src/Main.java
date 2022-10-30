19d3a77c326c87dd7af629e64108ddf765a4dd54
kamen <maks.kolesnikov.0202@mail.ru>
2022-10-30 16:30:06 +0300

    ну вроде все правильно, но теперь не понятно как  распичатать автора stalker

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Book stalker = new Book("S.T.A.L.K.E.R.", 2007, new Author("Sergey","Nedorubov" ));
-        Book witcher = new Book("Witcher",1986, new Author("Andrzej", "Sapkowski"));
+        Book stalker = new Book("S.T.A.L.K.E.R.", 2007, new Author("Sergey", "Nedorubov"));
         Author andrzej = new Author("Andrzej", "Sapkowski");
-        System.out.println(stalker.getName() + " " + stalker.getYearPublication() + " " +  );
-        System.out.println(witcher.getName() + " " + witcher.getYearPublication() + " " + andrzej.getName() + " " + andrzej.getMiddleName());
+        Book witcher = new Book("Witcher",1986, andrzej);
+        System.out.println(stalker.getBookName() + " " + stalker.getYearPublication() + " " + Author.getName() + " " + Author.getMiddleName()  );
+        System.out.println(witcher.getBookName() + " " + witcher.getYearPublication() + " " + andrzej.getName() + " " + andrzej.getMiddleName());
         witcher.setYearPublication(2013);
-        System.out.println(witcher.getName() + " " + witcher.getYearPublication() + " " + andrzej.getName() + " " + andrzej.getMiddleName());
+        System.out.println(witcher.getBookName() + " " + witcher.getYearPublication() + " " + andrzej.getName() + " " + andrzej.getMiddleName());
     }
 }
